3c99fce6a2b620be7f02b1b8433afe4a26bb735d
Oktay <oktayelb@gmail.com>
2025-12-15 16:37:35 +0300

    settings update for remote server rasp check. DUMMY COMMIT

--- config/settings.py
+++ config/settings.py
@@ -85,6 +85,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
+import os 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
