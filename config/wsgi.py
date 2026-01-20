a0b4a14711b7d7e6977af12a7a862e48191094fd
oktayelb <oktayelb@gmail.com>
2026-01-20 20:43:51 +0300

    Saving my changes to new branch

--- config/wsgi.py
+++ config/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
-
+##HELP
 import os
 
 from django.core.wsgi import get_wsgi_application
